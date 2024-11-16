16e1129474138edb1603929d80642e29faab09c1
parham ghasemi <parham.ghasemi.1388@gmail.com>
2024-11-17 01:24:39 +0330

    setup the Auth, Auth UI, Mongodb, prisma and NextAuth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,10 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [
+    require("../messenger-clone/node_modules/@tailwindcss/forms")({
+      strategy: 'class'
+    })
+  ],
 } satisfies Config;
