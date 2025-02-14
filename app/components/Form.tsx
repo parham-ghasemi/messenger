0d5bc504e114624a47280c9f57270eec11a14c37
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-02-14 17:43:21 +0330

    Enabled Sending Images

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -5,10 +5,9 @@ import axios from "axios";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { HiPaperAirplane, HiPhoto } from "react-icons/hi2";
 import MessageInput from "./MessageInput";
-import { useState } from "react";
-import UnAvalableModal from "@/app/components/UnAvalableModal";
 import { FullMesseageType } from "@/app/types";
 import { TiArrowBack } from "react-icons/ti"
+import { CldUploadButton } from "next-cloudinary";
 
 interface FormProps {
   replyToId: string | null;
@@ -20,7 +19,6 @@ interface FormProps {
 
 const Form: React.FC<FormProps> = ({ replyToId, setReplyToId, replyTo, replyVisible, setReplyVisible }) => {
   const { conversationId } = useConversation();
-  const [unAvalableOpen, setUnAvalableOpen] = useState(false);
 
   const { register, handleSubmit, setValue, formState: { errors } } = useForm<FieldValues>({
     defaultValues: {
@@ -37,35 +35,38 @@ const Form: React.FC<FormProps> = ({ replyToId, setReplyToId, replyTo, replyVisi
       });
   }
 
+  const handleUpload = (result: any) => {
+    axios.post('/api/messages', { image: result?.info?.secure_url, conversationId })
+  }
+
   const handleCancelReply = () => {
     setReplyToId(null)
     setReplyVisible(false)
   }
 
   return (
-    <>
-      <UnAvalableModal isOpen={unAvalableOpen} onClose={() => setUnAvalableOpen(false)} />
-      <div className=" py-4 px-4 bg-white border-t flex items-center gap-2 lg:gap-4 w-full">
-        <HiPhoto size={30} className="text-teal-500 cursor-pointer" onClick={() => setUnAvalableOpen(true)} />
-        <div className="w-full flex flex-col">
-          {
-            replyVisible && replyTo && (
-              <div onClick={handleCancelReply} className="cursor-pointer text-xs ms-3 pb-1 flex items-center text-teal-600 gap-1">
-                <TiArrowBack />
-                {replyTo?.body}
-              </div>
-            )
-          }
-          <form action="" onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 lg:gap-4 w-full">
-            <MessageInput id="message" register={register} errors={errors} required placeholder="write a message..." />
-            <button type="submit" className="rounded-full p-2 cursor-pointer hover:bg-teal-600 bg-teal-500 transition">
-              <HiPaperAirplane size={18} className="text-white" />
-            </button>
-          </form>
+    <div className=" py-4 px-4 bg-white border-t flex items-center gap-2 lg:gap-4 w-full">
+      <CldUploadButton options={{ maxFiles: 1 }} onUpload={handleUpload} uploadPreset="messenger" >
+        <HiPhoto size={30} className="text-teal-500 cursor-pointer" />
+      </CldUploadButton>
+      <div className="w-full flex flex-col">
+        {
+          replyVisible && replyTo && (
+            <div onClick={handleCancelReply} className="cursor-pointer text-xs ms-3 pb-1 flex items-center text-teal-600 gap-1">
+              <TiArrowBack />
+              {replyTo?.body}
+            </div>
+          )
+        }
+        <form action="" onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-2 lg:gap-4 w-full">
+          <MessageInput id="message" register={register} errors={errors} required placeholder="write a message..." />
+          <button type="submit" className="rounded-full p-2 cursor-pointer hover:bg-teal-600 bg-teal-500 transition">
+            <HiPaperAirplane size={18} className="text-white" />
+          </button>
+        </form>
 
-        </div>
       </div>
-    </>
+    </div>
   )
 }
 
